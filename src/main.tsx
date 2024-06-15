4864a8c4f2f19a5f97a501c3a2bd92fadec8df67
Yuri Cruz <yuricruzf@gmail.com>
2024-06-15 02:34:13 -0300

    Setting routes

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,21 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Movie from './pages/Movie.tsx'
+import Home from './pages/Home.tsx'
+import Search from './pages/Search.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route element={<App />}>
+          <Route path='/' element={<Home />} />
+          <Route path='movie/:id' element={<Movie />} />
+          <Route path='search' element={<Search />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
 )
