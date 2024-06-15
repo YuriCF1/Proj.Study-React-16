d337d7d55282d0a6677828b2042fc841c588811b
Yuri Cruz <yuricruzf@gmail.com>
2024-06-15 05:34:05 -0300

    Sending to saerch page

--- src/Components/NavBar.tsx
+++ src/Components/NavBar.tsx
@@ -1,17 +1,29 @@
 import { BiCameraMovie, BiSearchAlt2 } from "react-icons/bi"
 
-import { Link } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
 
 import styles from "./NavBarStyles.module.css"
+import { useState } from "react"
 
 const NavBar = () => {
+  const [search, setSearh] = useState("")
+  const navigate = useNavigate()
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    if (!search) return //Se não tiver nada no search, não redireciona
+    navigate("/search?query=" + search)
+    setSearh('')
+  }
+
   return (
     <nav className={styles.container} id="navbar">
       <h2>
         <Link to="/"><BiCameraMovie /></Link>
       </h2>
-      <form action="">
-        <input type="text" placeholder="Search a movie" name="" id="" />
+      <form action="" onSubmit={handleSubmit}>
+        <label htmlFor="Search">Search</label>
+        <input type="text" placeholder="a movie" name="Search" id="Search" onChange={(e) => setSearh(e.target.value)} value={search} />
         <button type="submit">
           <BiSearchAlt2 />
         </button>
