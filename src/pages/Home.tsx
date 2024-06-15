87f01fcfdc79546c56d7bb49925136aad2aaa93c
Yuri Cruz <yuricruzf@gmail.com>
2024-06-15 05:35:06 -0300

    I'll create a component of it later: Creating page bar

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -9,6 +9,20 @@ const apiKey = import.meta.env.VITE_API_KEY;
 const Home = () => {
   // const [topMovies, setTopMovies] = useState<ResultsEntity[]>() //Atribuindo o valor diretamente
   const [topMovies, setTopMovies] = useState<MovieResults | null>(null);
+  const [page, setPage] = useState(1);
+
+  const handlePages = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    const value = parseInt(e.currentTarget.value); // Obtém o valor do botão clicado e transforma para um numero
+    setPage((prevPage) => prevPage + value); // Usa a função de atualização do estado para garantir que está usando o valor mais recente de page
+    scrollToTop();
+  };
+
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
 
   const getTopRatedMovies = async (url: string) => {
     const res = await fetch(url);
@@ -19,9 +33,9 @@ const Home = () => {
   }
 
   useEffect(() => {
-    const topRatedUrl = `${moviesURL}top_rated?api_key=${apiKey}`;
+    const topRatedUrl = `${moviesURL}top_rated?api_key=${apiKey}&page=${page}`;
     getTopRatedMovies(topRatedUrl);
-  }, []);
+  }, [page]);
 
   return (
     <div className="container">
@@ -30,6 +44,29 @@ const Home = () => {
         {topMovies?.results ? topMovies?.results.length > 0 ? topMovies.results.map((movie) => (
           <MovieCard key={movie.id} movieSent={movie} />
         )) : <p>Loading...</p> : <p>Error... Please, try again later</p>}
+      </div>
+      <div className="pages-container">
+        {page > 2 && (
+          <>
+            <button onClick={(e) => handlePages(e)} value={-2}>
+              {page - 2}
+            </button>
+            <button onClick={(e) => handlePages(e)} value={-1}>
+              {page - 1}
+            </button>
+          </>
+        )}
+        <p>{page}</p>
+        {topMovies && topMovies?.total_pages > page &&
+          <>
+            <button onClick={(e) => handlePages(e)} value={1}>
+              {page + 1}
+            </button>
+            <button onClick={(e) => handlePages(e)} value={2}>
+              {page + 2}
+            </button>
+          </>
+        }
       </div>
     </div>
   )
--- src/pages/Search.tsx
+++ src/pages/Search.tsx
@@ -1,7 +1,88 @@
-const Search = () => {
-  return (
-    <div>Search</div>
-  )
-}
+import React, { useEffect, useState } from "react";
+import { useSearchParams } from "react-router-dom";
 
-export default Search
+import MovieCard from "../Components/MovieCard";
+import { MovieResults } from "../Interfaces/MoviesInterface";
+
+const searchUrl = import.meta.env.VITE_SEARCH;
+const apiKey = import.meta.env.VITE_API_KEY;
+
+const Search = () => {
+  const [searchParams] = useSearchParams(); //O SearchParams retorna um array
+  const [moviesFound, setMoviesFound] = useState<MovieResults | undefined>();
+  const [page, setPage] = useState(1);
+
+  const handlePages = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    const value = parseInt(e.currentTarget.value); // Obtém o valor do botão clicado e transforma para um numero
+    setPage((prevPage) => prevPage + value); // Usa a função de atualização do estado para garantir que está usando o valor mais recente de page
+    scrollToTop();
+  };
+
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
+
+  const query = searchParams.get("query");
+
+  const searchingMovies = async (url: string) => {
+    const res = await fetch(url);
+    const data: MovieResults = await res.json();
+
+    setMoviesFound(data);
+    console.log("MOVIES FOUND", data);
+  };
+
+  useEffect(() => {
+    const urlToSearch = `${searchUrl}?query=${query}&api_key=${apiKey}&page=${page}`;
+    searchingMovies(urlToSearch);
+  }, [query, page]);
+
+  return (
+    <div className="container">
+      <h2 className="title">
+        Results from: <span className="query-text">{query}</span>
+      </h2>
+      <div className="movies-container">
+        {moviesFound?.results ? (
+          moviesFound?.results.length > 0 ? (
+            moviesFound.results.map((movie) => (
+              <MovieCard key={movie.id} movieSent={movie} />
+            ))
+          ) : (
+            <p>Loading...</p>
+          )
+        ) : (
+          <p>Error... Please, try again later</p>
+        )}
+      </div>
+      <div className="pages-container">
+        {page > 2 && (
+          <>
+            <button onClick={(e) => handlePages(e)} value={-2}>
+              {page - 2}
+            </button>
+            <button onClick={(e) => handlePages(e)} value={-1}>
+              {page - 1}
+            </button>
+          </>
+        )}
+        <p>{page}</p>
+        {moviesFound && moviesFound?.total_pages > page &&
+          <>
+            <button onClick={(e) => handlePages(e)} value={1}>
+              {page + 1}
+            </button>
+            <button onClick={(e) => handlePages(e)} value={2}>
+              {page + 2}
+            </button>
+          </>
+        }
+      </div>
+    </div>
+  );
+};
+
+export default Search;
